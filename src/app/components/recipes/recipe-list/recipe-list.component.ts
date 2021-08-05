571a586a88e02ded616a28d5716930011009e12a
yusufozturk <yusufozturk2153@users.noreply.github.com>
2021-08-05 13:51:25 +0300

    Add header and recipe model Start recipe item component

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from 'src/app/models/recipe';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,6 +9,11 @@ import { Component, OnInit } from '@angular/core';
 export class RecipeListComponent implements OnInit {
 
   constructor() { }
+
+  recipes: Recipe[] = [
+    { name: "Test Name", description: "Test Description", imagePath: "https://img.buzzfeed.com/video-api-prod/assets/eb44570519264864814264f7f0a5e47a/BFV13909_BakedRatatouille-ThumbTextless1080.jpg?output-format=auto&output-quality=auto" },
+    { name: "Test Name 2", description: "Test Description 2", imagePath: "https://img.taste.com.au/vhZfDaJw/taste/2017/07/lemongrass-beef-bowl-128353-1.jpg" }
+  ];
 
   ngOnInit(): void {
   }
